e9ec867e57b828474b90db55ef91d25e4b37d838
xxmesldj <92644993+dskvksjv@users.noreply.github.com>
2024-02-18 16:40:19 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**навігаційна панель*/
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/*вдалий тест кейс*/
+const button = document.querySelector('.card-btn');
+
+button.addEventListener('click', function() {
+  window.location.href = 'https://moodle3.chmnu.edu.ua/';
+});
+
+/*невдалий тест кейс*/
+
+const aboutLink = document.querySelector('.navbar-link[href="#about"]');
+
+aboutLink.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  console.log('Link clicked but prevented default action.');
+});
+
+
+
+/*перемикання панелі навігації*/
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/**
+заголовок і go-top-btn активні
+коли вікно прокручується до 400 пікселів
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
